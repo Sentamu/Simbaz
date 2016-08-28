676728032f8a34ff9c4bdc387688cf2b380c9deb
sentamu Judah <sentsjman1@gmail.com>
2016-08-28 03:31:52 +0300

    lets see

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,13 +5,12 @@ class Listing < ActiveRecord::Base
 
 else
 	has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "img.jpg",
-	validates_attachment_content_type :image,:content_type => %w(image/jpeg image/jpg image/png)
 	
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :path =>":style/:id_:filename"
 
-    
+    validates_attachment_content_type :image,:content_type => %w(image/jpeg image/jpg image/png)
 
 end    
 
