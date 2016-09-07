115970b990879074d8e4dabe2b5e1eb032252d68
sentamu Judah <sentsjman1@gmail.com>
2016-09-07 18:39:32 +0300

    add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  get 'seller' => "listings#seller"
+
+  # The priorit y is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
