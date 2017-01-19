b45db14381f70572b473d26c1ee4cbfb94f83af2
sentamu Judah <sentsjman1@gmail.com>
2017-01-19 12:33:39 +0300

    failure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :tel])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :tel])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :avatar, :tel])
     
   end
