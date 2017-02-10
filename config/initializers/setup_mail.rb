cb4b8ddcec1692406b9845e0f090b11ba11cd797
sentamu Judah <sentsjman1@gmail.com>
2017-02-10 14:18:40 +0300

    new email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => 'smtp.gmail.com',
   :port                 => 587,
   :domain               => 'gmail.com',
-  :user_name            => 'sentsjman2@gmail.com',
-  :password             => 'sentrack',
+  :user_name            => 'sentams.com@gmail.com',
+  :password             => 'sentams1s',
   :authentication       => 'plain',
   :enable_starttls_auto => true
   }
