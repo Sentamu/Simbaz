e987379b567bad719f7893fe9010828bdff1c6b0
sentamu Judah <sentsjman1@gmail.com>
2017-03-01 23:21:18 +0300

    active admin pag

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -269,4 +269,7 @@ ActiveAdmin.setup do |config|
   # of those filters by default here.
   #
   # config.include_default_association_filters = true
+  Kaminari.configure do |config|
+  config.page_method_name = :per_page_kaminari
+end
 end
